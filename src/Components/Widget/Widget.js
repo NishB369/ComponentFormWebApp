50f21bd1b1f15bf022fdbe6fcad6fc4f6cf1bf74
nishb369 <nishbcodes@gmail.com>
2024-12-12 16:12:13 +0530

    initial commit 2

--- src/Components/Widget/Widget.js
+++ src/Components/Widget/Widget.js
@@ -1,22 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Widget = () => {
   return (
-    <div className= 'w-full border-2 border-black px-8 py-10 rounded-lg'>
-        <div>
+    <div className="w-full">
+      <div className="left_div">
+        <div className="w-full border-2 border-black px-8 py-4 rounded-lg">
+          <div className="main_heading">
             <h1>PW REAL Test Series for JEE and NEET</h1>
-        </div>
-        <div>
+          </div>
+          <div className="sub_heading">
             <h3>Right Evaluation & Assessment for Learning</h3>
-        </div>
-        <div className='main_btn'>
+          </div>
+          <div className="main_btn">
             <button>Register/Login</button>
-        </div>
-        <div className='share_btn'>
+          </div>
+          <div className="share_btn">
             <button>Share with Friends</button>
+          </div>
         </div>
+      </div>
+      <div className="right_div">
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Widget
+export default Widget;
--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Widget from './Components/Widget/Widget'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Widget from "./Components/Widget/Widget";
 
 const App = () => {
   return (
-    <div className='h-lvh w-[80%] flex items-center justify-center'>
-    <Widget />
+    <div className="h-lvh w-[100%] flex items-center justify-center">
+      <div className="w-[90%]">
+        <Widget />
+      </div>
     </div>
-)
-}
+  );
+};
 
-const root = ReactDOM.createRoot(document.querySelector("#root"))
-root.render(<App />)
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<App />);
